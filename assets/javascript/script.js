1df9f967fc99a34aba417fc911e7a7e28f7afa91
Aj-Herrera-99 <ajherrera999@gmail.com>
2024-11-19 15:16:16 +0100

    axios http request to jsonplaceholder (uncompleted)

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,6 +1,29 @@
 "use strict";
-// important ids
+// important ids classes
 const noteImageClass = "note-image";
+const figcaptionTag = "figcaption";
 // DOM elements selection
-const noteImage = document.querySelectorAll("."+noteImageClass);
-console.log(noteImage);
+const noteImages = document.querySelectorAll("." + noteImageClass);
+const figcaption = document.querySelectorAll(figcaptionTag);
+
+// http request with axios
+// URI
+const url = "https://jsonplaceholder.typicode.com/";
+const resource = "photos";
+const params = {
+    _limit: noteImages.length,
+};
+// axios api
+axios
+    .get(url + resource, { params, responseType: 'json' })
+    .then((res) => res.data)
+    .then((data) => {
+        console.log(data);
+        for (let i = 0; i < noteImages.length; i++) {
+            noteImages[i].src = data[i].url;
+            figcaption[i].innerHTML = data[i].title;
+        }
+    })
+    .catch((err) => {
+        console.error(err);
+    });
