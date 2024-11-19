8415fb82855e5a6ab8c51a9e8b5d62cfa50b9fea
Aj-Herrera-99 <ajherrera999@gmail.com>
2024-11-19 16:58:50 +0100

    click event on notes

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,17 +1,19 @@
 "use strict";
 // important ids classes tags
+const noteClass = ".note";
 const noteImageClass = ".note-image";
 const figcaptionTag = "figcaption";
 // DOM elements selection
-const noteImages = document.querySelectorAll(noteImageClass);
-const figcaptions = document.querySelectorAll(figcaptionTag);
+const $notes = document.querySelectorAll(noteClass);
+const $noteImages = document.querySelectorAll(noteImageClass);
+const $figcaptions = document.querySelectorAll(figcaptionTag);
 
 // http request with axios
 // URI
 const url = "https://jsonplaceholder.typicode.com/";
 const resource = "photos";
 const params = {
-    _limit: noteImages.length,
+    _limit: $noteImages.length,
 };
 // axios api
 axios
@@ -19,10 +21,18 @@ axios
     .then((res) => res.data)
     .then((data) => {
         for (let i = 0; i < data.length; i++) {
-            noteImages[i].src = data[i].url;
-            figcaptions[i].innerHTML = data[i].title;
+            $noteImages[i].src = data[i].url;
+            $figcaptions[i].innerHTML = data[i].title;
         }
     })
     .catch((err) => {
         console.error(err);
     });
+
+//* EVENT LISTENERS
+$noteImages.forEach((note) => note.addEventListener("click", handleNoteClick));
+
+//* EVENT HANDLERS
+function handleNoteClick(){
+    console.log(this);
+}
