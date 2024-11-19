8864aa5dd9973f6badf2bd008fc3192d892b1524
Aj-Herrera-99 <ajherrera999@gmail.com>
2024-11-19 14:48:39 +0100

    js dom elements selection

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,1 +1,6 @@
 "use strict";
+// important ids
+const noteImageClass = "note-image";
+// DOM elements selection
+const noteImage = document.querySelectorAll("."+noteImageClass);
+console.log(noteImage);
