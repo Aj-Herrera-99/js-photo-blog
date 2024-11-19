801907a8f91ca4bdbc1a03a04f1b3bd30776ce3e
Aj-Herrera-99 <ajherrera999@gmail.com>
2024-11-19 19:09:33 +0100

    refactoring js

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,5 +1,6 @@
 "use strict";
 // important ids classes tags
+const notesWrapperClass = ".notes-wrapper";
 const noteClass = ".note";
 const noteImageClass = ".note-image";
 const figcaptionTag = "figcaption";
@@ -11,66 +12,83 @@ const overflowHidden = "overflow-hidden";
 const $noteImages = document.querySelectorAll(noteImageClass);
 const $figcaptions = document.querySelectorAll(figcaptionTag);
 const $escape = document.getElementById(escapeId);
-const $notesWrapper = document.querySelector(".notes-wrapper");
+const $notesWrapper = document.querySelector(notesWrapperClass);
 // other variables
 let isModal = false;
 let escTimeout;
-
-// http request with axios
+const NOTES_COUNT = 20;
+// http request with axios, for generating notes
 // URI
 const url = "https://jsonplaceholder.typicode.com/";
 const resource = "photos";
 const params = {
-    _limit: 10,
+    _limit: NOTES_COUNT,
 };
-
-// axios api
 axios
     .get(url + resource, { params })
     .then((res) => res.data)
     .then((data) => {
-        let template = "";
-        for (let i = 0; i < data.length; i++) {
-            template += `<figure class="note d-flex flex-wrap">
-                    <div class="pin"><img src="./assets/img/pin.svg" alt="pin"></div>
-                    <img class="note-image" src="${data[i].url}" alt="img">
-                    <figcaption class="d-flex items-center">${data[i].title}</figcaption>
-                </figure>   `;
-        }
-        $notesWrapper.innerHTML = template;
-        const $notes = document.querySelectorAll(noteClass);
-        //* EVENT LISTENERS
-        $notes.forEach((note) =>
-            note.addEventListener("click", handleNoteClick)
-        );
+        generatesNotes($notesWrapper, data, data.length);
     })
     .catch((err) => {
         console.error(err);
     });
 
+// =============================================================================
+// ********************  EVENT LISTENERS  ************************************ 
+// =============================================================================
+// note event listener in axios generatesNotes()
 // escape key
 window.addEventListener("keyup", (e) => {
     if (e.key === "Escape" && isModal) {
-        document.body.classList.remove(layover);
-        document.body.classList.remove(overflowHidden);
-        document.querySelector(".modal").classList.remove("modal");
-        isModal = false;
-        // escape button
-        clearTimeout(escTimeout);
-        $escape.classList.remove("active");
+        triggerEscapeBtn();
+        isModal = triggerModalWindow(document.querySelector(".modal"), isModal);
     }
 });
 
-//* EVENT HANDLERS
+// =============================================================================
+// ********************  EVENT HANDLERS  ************************************ 
+// =============================================================================
 function handleNoteClick(e) {
     console.log(this);
-    if (!isModal) {
+    isModal = triggerModalWindow(this, isModal);
+}
+
+// =============================================================================
+//! ********************  FUNCTIONS  ************************************ 
+// =============================================================================
+function generatesNotes(parentElement, data, dataLen) {
+    let template = "";
+    for (let i = 0; i < dataLen; i++) {
+        template += `<figure class="note d-flex flex-wrap">
+                    <div class="pin"><img src="./assets/img/pin.svg" alt="pin"></div>
+                    <img class="note-image" src="${data[i].url}" alt="img">
+                    <figcaption class="d-flex items-center">${data[i].title}</figcaption>
+                </figure>   `;
+    }
+    parentElement.innerHTML = template;
+    const notes = document.querySelectorAll(noteClass);
+    //* note event listener
+    notes.forEach((note) => note.addEventListener("click", handleNoteClick));
+}
+
+function triggerModalWindow(target, modalState) {
+    triggerEscapeBtn();
+    if (!modalState) {
         document.body.classList.add(layover);
         document.body.classList.add(overflowHidden);
-        this.classList.add("modal");
-        // found on internet
-        document.body.scrollTop = document.documentElement.scrollTop = 0;
-        isModal = true;
+        target.classList.add("modal");
+        return true;
+    } else {
+        document.body.classList.remove(layover);
+        document.body.classList.remove(overflowHidden);
+        target.classList.remove("modal");
+        return false;
+    }
+}
+
+function triggerEscapeBtn() {
+    if ($escape.className.indexOf("active") !== 1) {
         // escape button
         $escape.classList.add("active");
         // dopo transitionTime ms, il button escape scompare
@@ -78,10 +96,6 @@ function handleNoteClick(e) {
             $escape.classList.remove("active");
         }, 2000);
     } else {
-        document.body.classList.remove(layover);
-        document.body.classList.remove(overflowHidden);
-        this.classList.remove("modal");
-        isModal = false;
         // escape button
         clearTimeout(escTimeout);
         $escape.classList.remove("active");
