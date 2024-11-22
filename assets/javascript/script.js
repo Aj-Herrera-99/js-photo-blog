e390d0ca4541f4062953c7d3148f407244b1eb12
Aj-Herrera-99 <ajherrera999@gmail.com>
2024-11-22 07:19:37 +0100

    modifiche importanti: 1. utilizzo di IIFEs per rimuovere dal global scope tutte quelle variabili il cui uso è strettamente legato ad una specifica funzione. 2. dalle IIFES ho creato dunque delle closures: variabili piu protette. 3.Refactoring massiccio per rendere il codice ancora piu modulare separando e raggruppando funzionalita e logiche diverse. 4. rimozioni di alcune variabili che indicavano un certo stato avente come scopo triggers di piu handlers (es. isModal, trashMode): invece di controllare lo stato, aggiungo e rimuovo in modo logico gli eventi. 5. nomenclatura migliorata.
    Prossimo step: organizzazione del javascript in piu files.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,15 +1,15 @@
-import random from "./utilities.js";
+import { getRndInteger as random } from "./utilities.js";
 // =============================================================================
-// ******************* INITIAL SETTINGS ******************************
+// ********************** INITIAL SETTING ***********************************
 // =============================================================================
-// constant
-const _URL = "https://jsonplaceholder.typicode.com/";
+// globals
 const _RESOURCE = "photos";
 const _KEY = "_limit";
+const _URL = "https://jsonplaceholder.typicode.com/";
 const _VALUE = 6;
 const _MAX_OBJECTS = 5000;
 // important ids, classes, tags selections
-const escapeModalBtnId = "escape-modal-btn";
+const popupModalBtnId = "popup-modal-btn";
 const addBtnId = "add-btn";
 const removeBtnId = "remove-btn";
 const escRemoveBtnId = "escape-remove-btn";
@@ -29,24 +29,19 @@ const hideParent = "hide-parent";
 const dNone = "d-none";
 const disabled = "disabled";
 // =============================================================================
-// ******************* STARTING POINT ******************************
+// ********************** STARTING POINT ***********************************
 // =============================================================================
 // DOM elements selection $prefix
 const $header = document.querySelector(headerTag);
 const $main = document.querySelector(mainTag);
 const $notesWrapper = document.querySelector(notesWrapperClass);
-const $escapeModalBtn = document.getElementById(escapeModalBtnId);
 const $addBtn = document.getElementById(addBtnId);
 const $removeBtn = document.getElementById(removeBtnId);
 const $escRemoveBtn = document.getElementById(escRemoveBtnId);
 // other variables
 const notesDataSaved = [];
-let trashMode = false;
-// for modal window and escape info message
-let isModal = false;
-let escTimeout;
 // media query list on toggling all hover effects
-const toggleHover = window.matchMedia("(max-width: 992px)");
+const toggleHover = window.matchMedia("(max-width: 1200px)");
 // run mediaQueryList listener function in run time
 handleMediaChange(toggleHover);
 // http request with axios, for generating notes
@@ -61,32 +56,19 @@ const params = {
     let myData = await getData(url + resource, params, notesDataSaved);
     // costruisco template a partire dai dati ricevuti e li inserisco in un contenitore
     buildTemplateFrom(myData, $notesWrapper);
-    // notes click event
-    // $notesWrapper
-    //     .querySelectorAll(noteClass)
-    //     .forEach((note) => note.addEventListener("click", handleNoteClick));
 })();
-// setInterval(()=> console.log("Test"), 1000); PROVA ASYNC
 // =============================================================================
 // ********************  EVENT LISTENERS  ************************************
 // =============================================================================
 // card wrapper click event delegation from note
-$notesWrapper.addEventListener("click", function (e) {
-    // il target cerca l elemento .note piu vicino ( se ce )
-    let note = e.target.closest(noteClass); 
-    if (!note) return; 
-    if (!this.contains(note)) return; 
-    note.scrollIntoView();
-    // faccio partire l'handler sul .note piu vicino al target
-    handleNoteClick(note); 
-});
+$notesWrapper.addEventListener("click", handleNoteClick);
 // add btn click event
 $addBtn.addEventListener("click", async (e) => {
     e.stopPropagation();
     let myData = await getData(
         url + resource,
         {
-            id: random(0, 100).toString(),
+            id: random(1, 100).toString(),
         },
         notesDataSaved
     );
@@ -98,7 +80,6 @@ $addBtn.addEventListener("click", async (e) => {
 // remove btn click event
 $removeBtn.addEventListener("click", function (e) {
     e.stopPropagation();
-    trashMode = true;
     this.innerText = "Clicca sulla polaroid da rimuovere";
     this.disabled = true;
     $addBtn.classList.add(dNone);
@@ -109,12 +90,12 @@ $removeBtn.addEventListener("click", function (e) {
     for (let note of $notesWrapper.children) {
         note.classList.add("constant-tilt-shake");
     }
-    window.addEventListener("click", handleRemoveNote);
+    $notesWrapper.removeEventListener("click", handleNoteClick);
+    $main.addEventListener("click", handleRemoveNote);
 });
 // escape remove btn click event
 $escRemoveBtn.addEventListener("click", function (e) {
     e.stopPropagation();
-    trashMode = false;
     $removeBtn.innerText = "Rimuovi una polaroid!";
     $removeBtn.disabled = false;
     $addBtn.classList.remove(dNone);
@@ -125,27 +106,10 @@ $escRemoveBtn.addEventListener("click", function (e) {
     for (let note of $notesWrapper.children) {
         note.classList.remove("constant-tilt-shake");
     }
-    window.removeEventListener("click", handleRemoveNote);
+    $notesWrapper.addEventListener("click", handleNoteClick);
+    $main.removeEventListener("click", handleRemoveNote);
 });
-// escape modal btn click event
-$escapeModalBtn.addEventListener("click", () => {
-    isModal = triggerModalWindow(
-        document.querySelector(`.${modal}`),
-        isModal,
-        trashMode
-    );
-});
-// window keyup event for escaping modal view
-window.addEventListener("keyup", (e) => {
-    // solo se ho cliccato ESC e l'overlay è attivo fai questo
-    if (e.key === "Escape" && isModal) {
-        isModal = triggerModalWindow(
-            document.querySelector(`.${modal}`),
-            isModal,
-            trashMode
-        );
-    }
-});
+
 // media query list change event
 toggleHover.addEventListener("change", function () {
     handleMediaChange(this);
@@ -154,8 +118,41 @@ toggleHover.addEventListener("change", function () {
 // =============================================================================
 // ********************  EVENT HANDLERS  ************************************
 // =============================================================================
-function handleNoteClick(target) {
-    isModal = triggerModalWindow(target, isModal, trashMode);
+function handleNoteClick(e) {
+    // il target cerca l elemento .note piu vicino ( se ce )
+    let note = e.target.closest(noteClass);
+    if (!note) return;
+    if (!this.contains(note)) return;
+    note.scrollIntoView();
+    // escape modal btn click event
+    const popupModalBtn = document.getElementById(popupModalBtnId);
+    // invoco funzione che gestisce la finestra modale prendendo
+    // come target note
+    // se il ritorno è true (apro modale), aggiungo ascoltatori
+    if (triggerModalWindow(note)) {
+        popupModalBtn.addEventListener("click", handleEscapeModal);
+        window.addEventListener("keyup", handleEscapeModal);
+    }
+    // se il ritorno è false (chiudo modale), rimuovo ascoltatori
+    else {
+        popupModalBtn.removeEventListener("click", handleEscapeModal);
+        window.removeEventListener("keyup", handleEscapeModal);
+    }
+}
+
+function handleEscapeModal(e) {
+    console.log("test");
+    const popupModalBtn = document.getElementById(popupModalBtnId);
+    if (e.currentTarget === popupModalBtn || e.key === "Escape") {
+        console.log("test");
+        const modalNote = document.querySelector(`.${modal}`);
+        if (!modalNote) return;
+        if (!document.contains(modalNote)) return;
+        if (!triggerModalWindow(modalNote)) {
+            popupModalBtn.removeEventListener("click", handleEscapeModal);
+            window.removeEventListener("keyup", handleEscapeModal);
+        }
+    }
 }
 
 function handleMediaChange(x) {
@@ -185,8 +182,66 @@ function handleRemoveNote(e) {
 }
 
 // =============================================================================
-//! ********************  FUNCTIONS  ************************************
+//! ********************  IIFEs - CLOSURES - FUNCTIONS  ***********************
 // =============================================================================
+const triggerModalWindow = (() => {
+    console.log("funzione esterna");
+    let isModal = false;
+    const popupModalBtn = document.getElementById(popupModalBtnId);
+    return (target) => {
+        console.log("closure");
+        // seleziono pin e figcaption della note target
+        const targetPin = target.querySelector(pinClass);
+        const targetFigcaption = target.querySelector(figcaptionTag);
+        // se non ho la modale aperta allora aprila
+        if (!isModal) {
+            isModal = true;
+            popupAnim(popupModalBtn, isModal);
+            // aggiungo le classi in funzione della finestra modale
+            document.body.classList.add(layover);
+            target.classList.add(modal);
+            // in modale, faccio scomparire il pin e figcaption
+            // e la cornice (il parent)
+            target.classList.add(hideParent);
+            targetPin.classList.add(dNone);
+            targetFigcaption.classList.add(dNone);
+            return isModal;
+        }
+        // se ho una modale aperta allora chiudila
+        else {
+            isModal = false;
+            console.log("Modal off");
+            popupAnim(popupModalBtn, isModal);
+            // rimuovo le classi in funzione della finestra modale
+            document.body.classList.remove(layover);
+            target.classList.remove(modal);
+            // se non ho la modale, faccio riapparire pin e figcaption e la cornice
+            target.classList.remove(hideParent);
+            targetPin.classList.remove(dNone);
+            targetFigcaption.classList.remove(dNone);
+            return isModal;
+        }
+    };
+})();
+
+const popupAnim = (() => {
+    let escTimeout;
+    return (popup, makeVisible) => {
+        if (makeVisible) {
+            // popup message
+            popup.classList.add(active);
+            // dopo tot ms, il message scompare
+            escTimeout = setTimeout(() => {
+                popup.classList.remove(active);
+            }, 2000);
+        } else {
+            // rimuovo (usando una classe) il message a tempo zero
+            clearTimeout(escTimeout);
+            popup.classList.remove(active);
+        }
+    };
+})();
+
 async function getData(completeUrl, params, saving) {
     try {
         const res = await axios.get(completeUrl, { params });
@@ -225,68 +280,5 @@ function buildTemplateFrom(data, wrapperElement) {
         wrapperElement.insertAdjacentHTML("beforeend", template);
     } else {
         console.error("No data found");
-    }
-}
-
-function triggerModalWindow(target, modalState, trashMode) {
-    // trashMode = false default
-    // trashMode è la var passata come param gestita da remove e escRemove btn
-    // trashMode = true solo quando clicco remove btn
-    // e entro dunque in modalita "rimuovi elemento" e ritorno false
-    // per non far partire l'animazione della modale
-    // escRemove esce dalla mod "rimuovi el" e resetta trashMode = false
-    // l'animazione puo partire
-    if (trashMode) {
-        return false;
-    }
-    // seleziono pin e figcaption della note target
-    const targetPin = target.querySelector(pinClass);
-    const targetFigcaption = target.querySelector(figcaptionTag);
-    // se non ho la modale aperta allora aprila
-    if (!modalState) {
-        // aggiungo le classi in funzione della finestra modale
-        document.body.classList.add(layover);
-        target.classList.add(modal);
-        // in modale, faccio scomparire il pin e figcaption
-        // e la cornice (il parent)
-        target.classList.add(hideParent);
-        targetPin.classList.add(dNone);
-        targetFigcaption.classList.add(dNone);
-        // chiamo la funzione che gestisce l'animazione di escape btn
-        animationEscapeModalBtn(modalState);
-        // ritorno true alla variabile isModal (vedere invocazione)
-        return true;
-    }
-    // se ho una modale aperta allora chiudila
-    else {
-        // rimuovo le classi in funzione della finestra modale
-        document.body.classList.remove(layover);
-        target.classList.remove(modal);
-        // se non ho la modale, faccio riapparire pin e figcaption e la cornice
-        target.classList.remove(hideParent);
-        targetPin.classList.remove(dNone);
-        targetFigcaption.classList.remove(dNone);
-        // chiamo la funzione che gestisce l'animazione di escape btn
-        animationEscapeModalBtn(modalState);
-        // ritorno false alla variabile isModal (vedere invocazione)
-        return false;
-    }
-    // funzione interna usata solo qui
-    function animationEscapeModalBtn(modalState) {
-        // se non ho la modale aperta, compare l'escape message
-        if (!modalState) {
-            // escape message
-            $escapeModalBtn.classList.add(active);
-            // dopo tot ms, il message scompare
-            escTimeout = setTimeout(() => {
-                $escapeModalBtn.classList.remove(active);
-            }, 2000);
-        }
-        // se ho una modale aperta, rimuovo forzatamente il timeout che rimuove il message dopo tot ms
-        else {
-            clearTimeout(escTimeout);
-            // rimuovo (usando una classe) il message a tempo zero
-            $escapeModalBtn.classList.remove(active);
-        }
     }
 }
--- assets/javascript/utilities.js
+++ assets/javascript/utilities.js
@@ -2,4 +2,4 @@ function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-export default getRndInteger;
+export {getRndInteger};
